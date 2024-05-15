afe9c70dd59bf4cab939ed2cb7e4e65502a5751a
petermusila <jmuxilaz@gmail.com>
2024-05-15 11:31:01 +0300

    correct errors

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -20,6 +21,7 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+
 export default function NavLinks() {
   const pathname = usePathname();
   return (
@@ -27,23 +29,22 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link href={''}>
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
                 'bg-sky-100 text-blue-600': pathname === link.href
-            },
+              }
             )}
-              >
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
           </Link>
         );
       })}
     </>
   );
 }
+
